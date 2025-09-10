68c17083c4620cbe6be0895c4bc39f2ef50af043
ankitkumar04100 <ankitkumarforall@gmail.com>
2025-09-10 23:35:46 +0530

    Create VolunteerDashboard.js

---
+++ frontend/src/components/VolunteerDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function VolunteerDashboard() {
+    return (
+        <div style={{margin: "20px"}}>
+            <h2>Volunteer Dashboard</h2>
+            <p>Tasks will appear here</p>
+        </div>
+    )
+}
+
+export default VolunteerDashboard;
